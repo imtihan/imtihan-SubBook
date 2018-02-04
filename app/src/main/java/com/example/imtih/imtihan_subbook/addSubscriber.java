9ac519524f03a9877428ebda4382a75c2b6ba5da
Imtihan Ahmed <imtihan@ualberta.ca>
2018-02-04 16:04:11 -0700

    Got the subscription page to work

--- app/src/main/java/com/example/imtih/imtihan_subbook/addSubscriber.java
+++ app/src/main/java/com/example/imtih/imtihan_subbook/addSubscriber.java
@@ -2,12 +2,26 @@ package com.example.imtih.imtihan_subbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class addSubscriber extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_subscriber);
+        setContentView(R.layout.new_subscription);
+    }
+
+    public void onAddSubscriberClick(View v){
+        if(v.getId() == R.id.AddSubscription){
+            EditText name = (EditText) findViewById(R.id.Name);
+            EditText date = (EditText) findViewById(R.id.Date);
+            EditText charge = (EditText) findViewById(R.id.Charge);
+            EditText comment = (EditText) findViewById(R.id.Comment);
+
+            String namestr = name.getText().toString();
+
+        }
     }
 }
